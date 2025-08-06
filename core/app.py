e505f61a6e9dd7b03dc6c3d8b1886557d7b7a6b2
hc.liu <hc.liu@suresofttech.com>
2025-08-06 09:00:25 +0800

    feat: add backend channel

--- core/app.py
+++ core/app.py
@@ -4,5 +4,7 @@ from PySide6.QtCore import QObject
 class App(QObject):
     def __init__(self, parent=None):
         super().__init__(parent)
-        # self._channel = Channel(0)
+
+        self._channels = [Channel(0), Channel(1), Channel(2)]
+
 
---
+++ core/channel.py
@@ -0,0 +1,6 @@
+from PySide6.QtCore import QObject
+
+class Channel(QObject):
+    def __init__(self, index, parent=None):
+        super().__init__(parent)
+        self._index = index;
