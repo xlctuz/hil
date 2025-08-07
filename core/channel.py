3eefabbdde9d0722db20db0e0b46c2bc74a44212
hc.liu <hc.liu@suresofttech.com>
2025-08-07 09:22:20 +0800

    refactor: 将 Project 和 Channel 模型与 QObject 解耦

--- core/channel.py
+++ core/channel.py
@@ -1,7 +1,7 @@
-from PySide6.QtCore import QObject
 
-class Channel(QObject):
-    def __init__(self, index, parent=None):
-        super().__init__(parent)
+class Channel():
+    #TODO 使用sqlalchemy 为Channel添加数据库类型, 并添加主键id字段
+    #TODO 实现跟Project的一对多关系, 一个通道包含多个项目
+    def __init__(self, index):
         self._index = index;
         self._projects = []
--- core/project.py
+++ core/project.py
@@ -1,7 +1,5 @@
-from PySide6.QtCore import QObject
 
 
-class Project(QObject):
-    def __init__(self, index, parent=None):
-        super().__init__(parent)
-        self._index = index;
+class Project():
+    def __init__(self):
+        pass
